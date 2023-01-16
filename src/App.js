1dd6d9fd2f76b5d3f86d49a399d7ce38767a4401
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2023-01-16 22:50:27 +0200

    feat: implement the ForwardCounter component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React from "react";
+
+import ForwardCounter from "./components/ForwardCounter";
+
 function App() {
-  return <div>GO!</div>;
+  return (
+    <React.Fragment>
+      <ForwardCounter />
+    </React.Fragment>
+  );
 }
 
 export default App;
